9610dc53efc330113ebc941696bc9ebea42b5adb
gnoblet <gnoblet@zaclys.net>
2025-04-15 15:09:38 +0200

    add deploy workflow

---
+++ src/HomePage.tsx
@@ -0,0 +1,34 @@
+import { lazy, Suspense } from "react";
+import { Helmet } from "react-helmet";
+import Preloader from "@/components/common/Preloader";
+
+const PageWrapper = lazy(() => import("@/components/layouts/PageWrapper"));
+const Navbar = lazy(() => import("@/components/layouts/Navbar"));
+const Hero = lazy(() => import("@/components/sections/Hero"));
+const FeaturedProjects = lazy(
+  () => import("@/components/sections/FeaturedProjects")
+);
+const Footer = lazy(() => import("@/components/layouts/Footer"));
+
+export default function HomePage(): JSX.Element {
+  return (
+    <>
+      <Helmet>
+        <title>Guillaume Noblet | Humanitarian Data Researcher</title>
+        <meta
+          name="description"
+          content="Guillaume Noblet is a Humanitarian Data Research at IMPACT Initiatives. Recently, I unpacked a new avenue I want to follow, viz & data journalism.."
+        />
+        <link rel="canonical" href="https://gnoblet.github.io" />
+      </Helmet>
+      <Suspense fallback={<Preloader />}>
+        <PageWrapper>
+          <Navbar />
+          <Hero />
+          <FeaturedProjects />
+          <Footer />
+        </PageWrapper>
+      </Suspense>
+    </>
+  );
+}
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,19 @@
+import { lazy, Suspense } from "react";
+import { Helmet } from "react-helmet";
+import Preloader from "@/components/common/Preloader";
+
+const NotFound = lazy(() => import("@/components/sections/NotFound"));
+
+export default function NotFoundPage(): JSX.Element {
+  return (
+    <>
+      <Helmet>
+        <title>Page Not Found | Guillaume Noblet</title>
+        <meta name="robots" content="noindex, nofollow" />
+      </Helmet>
+      <Suspense fallback={<Preloader />}>
+        <NotFound />
+      </Suspense>
+    </>
+  );
+}
